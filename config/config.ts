671ff9c870fbefa493a8e468b97fe8bf5b1e9df9
Alvaro <alvaro.gordillo.reina@gmail.com>
2021-03-15 22:46:26 +0100

    change config yaml

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,11 @@
 import * as yaml from 'js-yaml';
 import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const YAML_CONFIG_FILENAME = 'config.yaml';
 
 export default () => ({
-  configuration: yaml.load(readFileSync('config.yaml', 'utf8')),
+  configuration: yaml.load(
+    readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
+  ),
 });
